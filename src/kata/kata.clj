d630d3638e0e30965492d79eabc9ceab6c725d63
Yahya <yahyatinani@gmail.com>
2020-01-30 18:03:26 +0100

    Refactor

--- src/kata/kata.clj
+++ src/kata/kata.clj
@@ -1,19 +1,28 @@
 (ns kata.kata)
 
+(defn all-zero? [num1 num2]
+  (and (zero? num1) (zero? num2)))
+
+(defn square [x] (* x x))
+
+(defn discriminant [a b c]
+  (-
+    (square b)
+    (* 4 a c)))
+
+(defn real-sol [sign a b disc]
+  (/
+    (sign (- b) (Math/sqrt disc))
+    (* 2 a)))
+
 (defn quad [a b c]
-  (cond (and (zero? a) (zero? b))
+  (cond (all-zero? a b)
     []
-    
+
     (zero? a)
     [(/ (- b) c)]
 
     :else
-    (let [disc (-
-                 (* b b)
-                 (* 4 a c))]
-      [(/
-         (+ (- b) (Math/sqrt disc))
-         (* 2 a))
-       (/
-         (- (- b) (Math/sqrt disc))
-         (* 2 a))])))
+    (let [disc (discriminant a b c)]
+      [(real-sol + a b disc)
+       (real-sol - a b disc)])))
