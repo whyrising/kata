0c83ba76faf5df8745359962f2590648331cd19c
Yahya <yahyatinani@gmail.com>
2020-01-31 23:51:32 +0100

    Implement guess-to-base10-num function

--- src/mastermind/code_breaker.clj
+++ src/mastermind/code_breaker.clj
@@ -1,4 +1,7 @@
 (ns mastermind.code-breaker)
 
+(defn guess-to-base10-num [guess]
+  (reduce #(+ (* 6 %1) %2) guess))
+
 (defn break-code [past-guesses]
   [0 0 0 0])
--- test/mastermind/code_breaker_test.clj
+++ test/mastermind/code_breaker_test.clj
@@ -2,6 +2,18 @@
   (:require [midje.sweet :refer :all]
             [mastermind.code-breaker :refer :all]))
 
-(facts "Code Breaker"
-       (fact "initial guess"
-             (break-code []) => [0 0 0 0]))
+(facts
+  "Code Breaker"
+  (fact
+    "convert a guess (base 6 number) to base 10 number"
+    (guess-to-base10-num [0 0 0 0]) => 0
+    (guess-to-base10-num [0 0 0 4]) => 4
+    (guess-to-base10-num [0 0 1 0]) => 6
+    (guess-to-base10-num [0 0 1 1]) => 7
+    (guess-to-base10-num [0 1 1 1]) => 43
+    (guess-to-base10-num [1 1 1 1]) => 259
+    (guess-to-base10-num [5 5 5 5]) => 1295)
+
+  (fact
+    "initial guess"
+    (break-code []) => [0 0 0 0]))
