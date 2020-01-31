3ae44b32735917d84ff001c224861dbdb92a9cfd
Yahya <yahyatinani@gmail.com>
2020-01-31 23:07:59 +0100

    refactor over-count to use threading

--- src/mastermind/code_maker.clj
+++ src/mastermind/code_maker.clj
@@ -14,9 +14,10 @@
 
 (defn over-count [code guess]
   (let [code-values (set code)]
-    (reduce +
-            (filter pos? (map #(- (count-of % guess) (count-of % code))
-                              code-values)))))
+    (->> code-values
+         (map #(- (count-of % guess) (count-of % code)))
+         (filter pos?)
+         (reduce +))))
 
 (defn score [code guess]
   (let [p-score (position-matches code guess)
