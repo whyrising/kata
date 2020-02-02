0a0cb484ac853d37295f0a43b3cb91e75ecd8c47
Yahya <yahyatinani@gmail.com>
2020-02-02 20:41:09 +0100

    Refactor tests' comments

--- test/mastermind/code_breaker_test.clj
+++ test/mastermind/code_breaker_test.clj
@@ -5,7 +5,7 @@
 (facts
   "Code Breaker"
   (fact
-    "convert a guess (base 6 number) to base 10 number"
+    "Convert a guess (base 6 number) to base 10 number."
     (guess-to-base10-num [0 0 0 0]) => 0
     (guess-to-base10-num [0 0 0 4]) => 4
     (guess-to-base10-num [0 0 1 0]) => 6
@@ -15,11 +15,11 @@
     (guess-to-base10-num [5 5 5 5]) => 1295)
 
   (fact
-    "when number is bigger than 4 digits, return :next-guess-out-of-range"
+    "When number is bigger than 4 digits, return :next-guess-out-of-range."
     (base10-num-to-guess (* 6 6 6 6)) => :guess-out-of-range)
 
   (fact
-    "convert a base 10 number to a, base 6, 4 digits, number guess)"
+    "Convert a base 10 number to a, base 6, 4 digits guess."
     (base10-num-to-guess 0) => [0 0 0 0]
     (base10-num-to-guess 4) => [0 0 0 4]
     (base10-num-to-guess 6) => [0 0 1 0]
@@ -29,34 +29,33 @@
     (base10-num-to-guess 1295) => [5 5 5 5])
 
   (fact
-    "increment guess"
+    "Increment guess."
     (inc-guess [0 0 0 0]) => [0 0 0 1]
     (inc-guess [0 0 0 5]) => [0 0 1 0]
     (inc-guess [0 0 5 5]) => [0 1 0 0]
     (inc-guess [0 5 5 5]) => [1 0 0 0])
 
   (fact
-    "initial guess"
+    "Initial guess."
     (break-code nil []) => [0 0 0 0])
 
   (fact
-    "first step for code [1 2 3 4]"
+    "First step for code [1 2 3 4]."
     (break-code [0 0 0 0]
                 [[[0 0 0 0] [0 0]]]) => [1 1 1 1])
 
   (fact
-    "first step for code [0 0 0 1]"
+    "First step for code [0 0 0 1]."
     (break-code [0 0 0 0]
                 [[[0 0 0 0] [3 0]]]) => [0 0 0 1])
 
   (fact
-    "second step for code [0 0 1 0]"
+    "Second step for code [0 0 1 0]."
     (break-code [0 0 0 1]
                 [[[0 0 0 1] [2 2]]]) => [0 0 1 0])
 
   (fact
-    "two steps are required for [0 0 1 0]"
+    "Two steps are required for [0 0 1 0]."
     (break-code [0 0 0 0]
                 [[[0 0 0 0] [3 0]]
-                 [[0 0 0 1] [2 2]]]) => [0 0 1 0])
-  )
+                 [[0 0 0 1] [2 2]]]) => [0 0 1 0]))
--- test/mastermind/code_maker_test.clj
+++ test/mastermind/code_maker_test.clj
@@ -3,43 +3,43 @@
             [mastermind.code-maker :refer :all]))
 
 (facts
-  "Scoring Position Matches"
+  "Scoring Position Matches."
   (fact
-    "score guess with no matches"
+    "Score guess with no matches."
     (score [0 0 0 0] [1 1 1 1]) => [0 0])
 
   (fact
-    "score guess with one :pos match"
+    "Score guess with one :pos match."
     (score [0 0 0 0] [0 1 1 1]) => [1 0])
 
   (fact
-    "score guess with two :pos match"
+    "Score guess with two :pos match."
     (score [0 0 0 0] [0 1 1 0]) => [2 0])
 
   (fact
-    "score guess with many position matches"
+    "Score guess with many position matches."
     (score [0 0 0 0] [0 1 0 0]) => [3 0]
     (score [0 0 0 0] [1 0 0 0]) => [3 0]
     (score [0 0 0 0] [0 0 0 0]) => [4 0]))
 
 (facts
   "Scoring Value Matches. Matches that have the right value, but are in the
-  wrong position"
+  wrong position."
   (fact
-    "value matches"
+    "Value matches."
     (score [1 2 3 4] [2 0 0 0]) => [0 1]
     (score [1 2 3 4] [2 3 0 0]) => [0 2]
     (score [1 2 3 4] [2 3 1 0]) => [0 3]
     (score [1 2 3 4] [2 3 4 1]) => [0 4]))
 
 (fact
-  "Guesses with some position and some value matches"
+  "Guesses with some position and some value matches."
   (score [1 2 3 4] [1 2 4 3]) => [2 2])
 
 (fact
-  "when there are duplicate colors in the guess, they cannot all be
+  "When there are duplicate colors in the guess, they cannot all be
   awarded/scored unless they correspond to the same number of duplicates colors
-  in the hidden code"
+  in the hidden code."
   (score [1 2 3 4] [3 3 3 4]) => [2 0]
   (score [1 1 3 4] [1 3 1 3]) => [1 2]
   (score [3 3 3 3] [3 1 1 1]) => [1 0])
